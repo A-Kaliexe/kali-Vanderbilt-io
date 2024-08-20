b3840da89f73c6b452bfab3432bc9012bc832739
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-08-20 14:54:54 -0500

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const footer = document.createElement("footer");
+/*Date-Year*/
+const today = new Date(); // Fixing the Date object
+const thisYear = today.getFullYear();
+
+let copyright = document.createElement("p");
+copyright.innerHTML = `&copy; Kali Vanderbilt ${thisYear}`;
+
+footer.appendChild(copyright);
+const rightContent = document.querySelector(".right-content");
+rightContent.appendChild(footer);
+
+/*Skills*/
+const skills = ["JavaScript", "HTML", "CSS", "Canva", "GitHub"];
+const skillsSection = document.querySelector("#Skills");
+const skillsList = document.createElement("ul");
+
+skills.forEach((skillText) => {
+  const skill = document.createElement("li");
+  skill.innerText = skillText;
+  skillsList.appendChild(skill);
+});
+
+skillsSection.appendChild(skillsList);
